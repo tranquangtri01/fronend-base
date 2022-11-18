182a188de392bb6bd16cef3ea1cc00b0b7788298
tranquangtri01 <tritran21012001@gmail.com>
2022-11-18 17:56:19 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HeaderOnly from "./components/layout/Herder/herder";
+
+function App() {
+  return (
+    <div className="App">
+    <HeaderOnly/>
+      <h1>Hello Word</h1>
+    </div>
+  );
+}
+
+export default App;
